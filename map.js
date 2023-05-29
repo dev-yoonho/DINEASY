d02e18ea0b96d5028fc85360673978251086e324
Yoonho Cho <124619604+dev-yoonho@users.noreply.github.com>
2023-05-30 01:41:01 +0900

    현재 위치를 지도 상에 표현하도록 수정 - 테스트용

--- map.js
+++ map.js
@@ -1,38 +1,57 @@
-var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
-var options = { //지도를 생성할 때 필요한 기본 옵션
-	center: new kakao.maps.LatLng(37.56004728498993, 126.93688499998413), //지도의 중심좌표.
+var mapContainer = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+var mapOption = { //지도를 생성할 때 필요한 기본 옵션
+	center: new kakao.maps.LatLng(37.56004728498993, 126.93688499998413), //지도의 중심좌표(연세대학교 교차로)
 	level: 3 //지도의 레벨(확대, 축소 정도)
 };
 
-function currentLocation() {
-	// HTML5의 geolocation으로 사용할 수 있는지 확인합니다
-	if (navigator.geolocation) {
+var map = new kakao.maps.Map(mapContainer, mapOption); //지도 생성 및 객체 리턴
 
-		// GeoLocation을 이용해서 접속 위치를 얻어옵니다
-		navigator.geolocation.getCurrentPosition(function(position) {
-
-			var lat = position.coords.latitude, // 위도
-			    lon = position.coords.longitude; // 경도
-
-			var locPosition = new kakao.maps.LatLng(lat, lon); // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
-			var message = '<div style="padding:5px;">현위치</div>'; // 인포윈도우에 표시될 내용입니다
-
-			// 마커와 인포윈도우를 표시합니다
-			displayMarker(locPosition, message);
-		});
-	} else { // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
-
-		var locPosition = new kakao.maps.LatLng(37.4812845080678, 126.952713197762),
-			message = '현재 위치를 알 수 없어 기본 위치로 이동합니다.'
-
-		currentLatLon['lat'] = 33.450701
-		currentLatLon['lon'] = 126.570667
-
-		displayMarker(locPosition, message);
-	}
-	return true
+// HTML5의 geolocation으로 사용할 수 있는지 확인합니다 
+if (navigator.geolocation) {
+    
+    // GeoLocation을 이용해서 접속 위치를 얻어옵니다
+    navigator.geolocation.getCurrentPosition(function(position) {
+        
+        var lat = position.coords.latitude, // 위도
+            lon = position.coords.longitude; // 경도
+        
+        var locPosition = new kakao.maps.LatLng(lat, lon), // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
+            message = '<div style="padding:5px;">현재 위치</div>'; // 인포윈도우에 표시될 내용입니다
+        
+        // 마커와 인포윈도우를 표시합니다
+        displayMarker(locPosition, message);
+            
+      });
+    
+} else { // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
+    
+    var locPosition = new kakao.maps.LatLng(33.450701, 126.570667),    
+        message = 'geolocation을 사용할수 없어요..'
+        
+    displayMarker(locPosition, message);
 }
 
-currentLocation();
+// 지도에 마커와 인포윈도우를 표시하는 함수입니다
+function displayMarker(locPosition, message) {
 
-var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+    // 마커를 생성합니다
+    var marker = new kakao.maps.Marker({  
+        map: map, 
+        position: locPosition
+    }); 
+    
+    var iwContent = message, // 인포윈도우에 표시할 내용
+        iwRemoveable = true;
+
+    // 인포윈도우를 생성합니다
+    var infowindow = new kakao.maps.InfoWindow({
+        content : iwContent,
+        removable : iwRemoveable
+    });
+    
+    // 인포윈도우를 마커위에 표시합니다 
+    infowindow.open(map, marker);
+    
+    // 지도 중심좌표를 접속위치로 변경합니다
+    map.setCenter(locPosition);      
+}    
