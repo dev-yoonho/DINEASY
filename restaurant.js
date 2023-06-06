fd5980688978950a64799f1879ab50651e4d5c6f
2022148072 <128347728+2022148072@users.noreply.github.com>
2023-06-06 17:26:22 +0900

    Add files via upload

---
+++ restaurant.js
@@ -0,0 +1,84 @@
+var category = window.localStorage.getItem('category');
+
+function changeCategory(value) {
+    let result = value;
+    switch(value) {
+        case '중식':
+            result = '중국식';
+            break;
+        case '양식':
+            result = '경양식';
+            break;
+    }
+    return result;
+}
+
+let showNum = 8;
+const scrollAddition = 4;
+let restaurants;
+let categoryRests;
+
+(async () => {
+    restaurants = await getRestaurants();
+
+    categoryRest();
+    scrollToEnd();
+})()
+
+async function getRestaurants() {
+    let res = await fetch('./restaurant.json');
+
+    return res.DATA.json();
+}
+
+function categoryRest() {
+
+    let filtered = restaurants.filter(restaurant => restaurant.uptaenm == changeCategory(category));
+    categoryRests.push(...filtered);
+
+    console.log(categoryRests);
+
+    show(categoryRests, showNum);
+}
+
+function show(rests, num) {
+    document.querySelector('header').innerText = category + '음식';
+
+    let parent = document.querySelector('parent');
+
+    for (let rest of rests.slice(0, num)) {
+
+        let box = document.createElement('button');
+        box.classList.add('buttonRest');
+
+        let img = document.createElement('img');
+        img.scr = '';
+        img.alt = rest.bplcnm;
+        img.classList.add('image-icon');
+
+        let name = document.createElement('b');
+        name.innerText = rest.bplcnm;
+
+        let info = documnet.createElement('p');
+        info.innerText = '종류: ' + rest.uptaenm + '\n' + '주소: ' + rest.sitewhladdr;
+
+        box.appendChild(name);
+        box.appendChild(img);
+        box.appendChild(info);
+
+        parent.appendChild(box);
+
+    }
+}
+
+function scrollToEnd() {
+    window.scroll = () => {
+        let val = window.scrollY + window.innerHeight - document.body.offsetHeight;
+
+        if (val == 0) {
+            console.log(true);
+            showNum += scrollAddition;
+            show(categoryRests, showNum);
+        }
+    }
+}
