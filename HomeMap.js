b8ae6b6769a782d18bda9d36b219c7096d7f5267
Yoonho Cho <124619604+dev-yoonho@users.noreply.github.com>
2023-06-10 10:33:02 +0900

    Update HomeMap.js

--- HomeMap.js
+++ HomeMap.js
@@ -1,5 +1,16 @@
 var map;
 
+if ("geolocation" in navigator) {
+  navigator.geolocation.getCurrentPosition((position) => {
+    const latHome = position.coords.latitude;
+    const lonHome = position.coords.longitude;
+    console.log("현재 위치의 좌표 - 위도:", latHome, "경도:", lonHome);
+    reverseGeo(lonHome, latHome);
+  });
+} else {
+  console.log("Geolocation을 지원하지 않는 브라우저입니다.");
+}
+
 // 페이지가 로딩이 된 후 호출하는 함수입니다.
 function initTmap() {
   // map 생성
@@ -71,4 +82,93 @@ function displayMaker(locPosition) {
 remove();
 function remove() {
   map.setOptions({ zoomControl: false }); // 지도 옵션 줌컨트롤 표출 비활성화
+}
+
+function reverseGeo(lon, lat) {
+  var headers = {};
+  headers["appKey"] = "KThdzstXSE8XxtqffJ4IC5eV2M9jBSvH59JYlmWW";
+
+  $.ajax({
+    method: "GET",
+    headers: headers,
+    url: "https://apis.openapi.sk.com/tmap/geo/reversegeocoding?version=1&format=json&callback=result",
+    async: false,
+    data: {
+      coordType: "WGS84GEO",
+      addressType: "A10",
+      lon: lon,
+      lat: lat,
+    },
+    success: function (response) {
+      // 3. json에서 주소 파싱
+      var arrResult = response.addressInfo;
+
+      //법정동 마지막 문자
+      var lastLegal = arrResult.legalDong.charAt(
+        arrResult.legalDong.length - 1
+      );
+
+      // 새주소
+      newRoadAddr = arrResult.city_do + " " + arrResult.gu_gun + " ";
+
+      if (
+        arrResult.eup_myun == "" &&
+        (lastLegal == "읍" || lastLegal == "면")
+      ) {
+        //읍면
+        newRoadAddr += arrResult.legalDong;
+      } else {
+        newRoadAddr += arrResult.eup_myun;
+      }
+      newRoadAddr += " " + arrResult.roadName + " " + arrResult.buildingIndex;
+
+      // 새주소 법정동& 건물명 체크
+      if (arrResult.legalDong != "" && lastLegal != "읍" && lastLegal != "면") {
+        //법정동과 읍면이 같은 경우
+
+        if (arrResult.buildingName != "") {
+          //빌딩명 존재하는 경우
+          newRoadAddr +=
+            " (" + arrResult.legalDong + ", " + arrResult.buildingName + ") ";
+        } else {
+          newRoadAddr += " (" + arrResult.legalDong + ")";
+        }
+      } else if (arrResult.buildingName != "") {
+        //빌딩명만 존재하는 경우
+        newRoadAddr += " (" + arrResult.buildingName + ") ";
+      }
+
+      // 구주소
+      jibunAddr =
+        arrResult.city_do +
+        " " +
+        arrResult.gu_gun +
+        " " +
+        arrResult.legalDong +
+        " " +
+        arrResult.ri +
+        " " +
+        arrResult.bunji;
+      //구주소 빌딩명 존재
+      if (arrResult.buildingName != "") {
+        //빌딩명만 존재하는 경우
+        jibunAddr += " " + arrResult.buildingName;
+      }
+
+      console.log(jibunAddr);
+      localStorage.setItem("userLocation", jibunAddr);
+    },
+    error: function (request, status, error) {
+      console.log(
+        "code:" +
+          request.status +
+          "\n" +
+          "message:" +
+          request.responseText +
+          "\n" +
+          "error:" +
+          error
+      );
+    },
+  });
 }
